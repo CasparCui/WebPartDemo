97d0fbfefc9432202ce1836a978128f8c12ac3d2
hwcui <hwcui@WIN-2D10C362GIE>
2018-09-25 21:09:01 +0900

    WebService Demo

---
+++ WebServiceDemo1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+
+namespace WebServiceDemo1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            SharePointWebService.Lists listService = new SharePointWebService.Lists();
+            listService.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            var xmlDoc = new XmlDocument();
+            var itemQuery = xmlDoc.CreateNode(XmlNodeType.Element, "Query", string.Empty);
+            var itemQueryOptions = xmlDoc.CreateNode(XmlNodeType.Element, "QueryOptions", string.Empty);
+            var itemViewFields = xmlDoc.CreateNode(XmlNodeType.Element, "ViewFields", string.Empty);
+            itemQueryOptions.InnerXml = @"<IncludeMandatoryColumns>False</IncludeMandatoryColumns> <DataInUtc>True</DataInUtc>";
+            itemViewFields.InnerXml = @"<FieldRef Name = 'Title'/>";
+            itemQuery.InnerXml = @"<Where><Eq><FieldRef Name='Title' /><Value Type='Text'>fff</Value></Eq></Where>";
+
+
+            var node = listService.GetListItems("test List", null, itemQuery, itemViewFields, null, itemQueryOptions, null);
+
+            var nodes = (node as XmlElement).GetElementsByTagName(@"z:row");
+            foreach (XmlElement subNode in nodes)
+            {
+                using (var fs = File.Open(@"c:\item.xml", FileMode.Open, FileAccess.Read))
+                {
+                    var array = new byte[fs.Length];
+                    fs.ReadAsync(array, 0, Convert.ToInt32(fs.Length), System.Threading.CancellationToken.None);
+
+
+                    var listId = subNode.GetAttributeNode("ows_ID").Value;
+                    listService.AddAttachment("test List", listId, fs.Name, array);
+                }
+            }
+
+
+        }
+    }
+}
